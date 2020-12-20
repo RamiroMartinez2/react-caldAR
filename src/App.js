47c6bf5fbc4c2d3958946db6f171db036439247f
Alejandro Lupo <alejandro.lupo@radiumrocket.com>
2020-12-20 12:14:27 -0300

    fix eslint file, fix indentation, etc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import "./App.css";
-import Nav from './Layout/Nav/Nav';
-import Main from './Layout/Main/Main';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./Layout/Nav/Nav";
+import Main from "./Layout/Main/Main";
 
 const App = () => {
-
   return (
     <Router>
       <div className="App">
-        <Nav/>
+        <Nav />
         <Switch>
-          <Route path="/" exact component={Main}/>
+          <Route path="/" exact component={Main} />
         </Switch>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
-function Home () {
-
-    return (
-        <div className="title">
-            <h1>CaldAR</h1>
-            <h2>The Best Choice</h2>
-      </div>
-    )
+const Home = () => {
+  return (
+    <div className="title">
+      <h1>CaldAR</h1>
+      <h2>The Best Choice</h2>
+    </div>
+  );
 }
 
 export default Home;
