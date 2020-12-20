cc3151322fdd9e2cf454e9daedc0a949d9edb01d
cocolobos <nicolasmlobos@gmail.com>
2020-12-20 18:08:06 -0300

    CAL-35-Technicians-Route

---
+++ src/Components/Technicians/HeaderTechnician/HeaderTechnician.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HeaderTechnician.css'
+
+function Header() {
+    return(
+        <header className="headerStyle">
+            <h1>Technicians</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Technicians/MainTechnician/MainTechnician.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Tech from '../Tech/Tech';
+import AddTechnician from '../AddTechnician/AddTechnician';
+import HeaderTechnician from '../HeaderTechnician/HeaderTechnician';
+import mockTechnicians from '../../../Mock/mocksTechnician.json';
+import nextId from 'react-id-generator';
+import './MainTechnician.css';
+
+class MainTechnician extends Component {
+  state = {mockTechnicians}
+  
+  delTech = (number) =>{
+    this.setState({mockTechnicians: [...this.state.mockTechnicians.filter(tech => tech.number !== number)] })
+  }
+
+  updateTechnician = (techUpdated) => {
+    this.setState({
+      mockTechnicians: [...this.state.mockTechnicians.map(tech => {
+        if(tech.number === techUpdated.number) {
+          tech = techUpdated;
+        }
+        return tech;
+      })]
+    });
+  }
+
+  addTech = ({fullName, email, phone, statusActive, trained, assignedClients, spareHoursAvailable}) => {
+    const newTech = {
+      number: nextId(),
+      fullName,
+      email,
+      phone,
+      statusActive,
+      trained,
+      assignedClients,
+      spareHoursAvailable
+    }
+    this.setState({mockTechnicians: [...this.state.mockTechnicians, newTech]})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <HeaderTechnician />
+          <Tech 
+            technicians={this.state.mockTechnicians} 
+            delTech={this.delTech} 
+            updateTechnician={this.updateTechnician} 
+          />
+          <AddTechnician  addTech={this.addTech}/>
+        </div>
+      </div>
+    );
+  } 
+}
+
+export default MainTechnician;
