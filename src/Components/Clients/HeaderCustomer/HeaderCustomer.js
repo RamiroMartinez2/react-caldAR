fb4abd79c3031af2aaf182e1ef77919c985fe7ad
LEN-PC0B7SP5\ramir <ramiro.rc.rm@gmail.com>
2020-12-20 19:29:10 -0300

    fix eslint file,etc

---
+++ src/Components/Clients/HeaderCustomer/HeaderCustomer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './HeaderCustomer.css'
+
+function HeaderCustomer() {
+  return (
+    <header className="headerStyle">
+      <h1>Customers</h1>
+    </header>
+  );
+}
+
+export default HeaderCustomer;
