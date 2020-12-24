ad508409b33004323112add2d408db1c3a0991c7
Alexandra Maero <alexandramaero12@gmail.com>
2020-12-24 14:09:44 -0300

    add initial structure of Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Routes from './App'
+import rootReducer from './redux/reducers/rootReducer.js'
 import "./index.css";
 import App from "./App";
 
+const configureStore = () => {
+  const enhancer = composeWithDevTools();
+  return createStore(rootReducer, enhancer);
+} 
+
+const store = configureStore
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <Routes />
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
