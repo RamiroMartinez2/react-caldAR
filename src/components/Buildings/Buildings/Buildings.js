3ec0ab6365d372f541bc690fd93fe431e3596f2a
Alejandro Lupo <alejandro.lupo@radiumrocket.com>
2020-12-24 16:58:37 -0300

    CAL-37: update folder mocks

--- src/components/Buildings/Buildings/Buildings.js
+++ src/components/Buildings/Buildings/Buildings.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Bld from "../Bld/Bld";
 import AddBuildings from "../AddBuildings/AddBuildings";
 import HeaderBuilding from "../HeaderBuildings/HeaderBuildings";
-import mockBuildings from "../../../Mocks/buildings.json";
+import mockBuildings from "../../../mocks/buildings.json";
 import { v4 as uuidv4 } from "uuid";
 import "./Buildings.css";
 
