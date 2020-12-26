ddcf064aeec417f5ad56e37b6343da1d3a3c1612
Alexandra Maero <alexandramaero12@gmail.com>
2020-12-26 10:07:31 -0300

    folder structure to use for Redux implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import Routes from './App'
 import rootReducer from './redux/reducers/rootReducer.js'
 import "./index.css";
 import App from "./App";
@@ -19,10 +17,7 @@ const store = configureStore
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Routes />
         <App />
-      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
