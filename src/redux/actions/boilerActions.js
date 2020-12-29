f05eafed4ac58a2e095d8a03a90ba50a9dab866b
Alexandra Maero <alexandramaero12@gmail.com>
2020-12-29 10:40:28 -0300

    CAL-59: resolve conflicts in .eslintcache

--- src/redux/actions/boilerActions.js
+++ src/redux/actions/boilerActions.js
@@ -1,4 +1,35 @@
-import { ADD_BOILER, DELETE_BOILER, EDIT_BOILER } from "../types/boilerTypes";
+import { ADD_BOILER, 
+  DELETE_BOILER, 
+  EDIT_BOILER, 
+  GET_BOILERS_FETCHING, 
+  GET_BOILERS_FULFILLED } from "../types/boilerTypes";
+
+const URL = 'mongodb+srv://radium-rocket:radium1234@caldar-cluster.763oz.mongodb.net/CaldAR?retryWrites=true&w=majority'
+
+export const getBoilersFetching = () => ({
+  type: GET_BOILERS_FETCHING
+});
+
+export const getBoilersFulfilled = (payload) => ({
+  type: GET_BOILERS_FULFILLED,
+  payload
+});
+
+export const getBoilersRejected = () => ({
+  type: GET_BOILERS_REJECTED
+});
+
+export const getBoilers = () => dispatch => {
+  dispatch(getBoilersFetching());
+  return fetch(URL)
+  .then(data => data.json())
+  .then(response => {
+    dispatch(getBoilersFulfilled(response));
+  }) 
+  .catch(() => {
+    dispatch(getBoilersRejected());
+  });
+};
 
 export const addBoiler = content => ({
   type: ADD_BOILER,
