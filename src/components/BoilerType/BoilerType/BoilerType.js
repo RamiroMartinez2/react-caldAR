a24446c68c868587dfc8fc88d360375174f330ef
Alexandra Maero <alexandramaero12@gmail.com>
2020-12-29 14:59:05 -0300

    CAL-58: add boilerTypes actions, types and reducers

--- src/components/BoilerType/BoilerType/BoilerType.js
+++ src/components/BoilerType/BoilerType/BoilerType.js
@@ -1,36 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 import ListBoilerType from "../ListBoilerType/ListBoilerType";
 import style from './BoilerType.module.css'
 
-class BoilerType extends Component {
-  render() {
-    return (
-      <div>
-        <ul className={style.ulStyle}>
-            <li className={style.liStyleHeader}>Id</li>
-            <li className={style.liStyleHeader}>Skills Id</li>
-            <li className={style.liStyleHeader}>Description</li>
-            <li className={style.liStyleHeader}>Stock</li>            
-            <li className={style.liStyleHeader}>Actions</li>
-          </ul>
-      {this.props.boilerList.map((list) => (
-      <ListBoilerType
-        key={list.id}
-        list={list}
-        delBoilerType={this.props.delBoilerType}
-        updateBoilerType={this.props.updateBoilerType}
-      />
-      ))}
-      </div>
-    );
-  }
+const BoilerType = (props) => {
+
+  const listBoilerType = props.boilerTypes.map((boilerType) => (
+    <ListBoilerType
+      key={boilerType.id}
+      boilerType={boilerType}
+    />
+  ));
+  return (
+    <div>
+      <ul className={style.ulStyle}>
+          <li className={style.liStyleHeader}>Id</li>
+          <li className={style.liStyleHeader}>Skills Id</li>
+          <li className={style.liStyleHeader}>Description</li>
+          <li className={style.liStyleHeader}>Stock</li>            
+      </ul>
+      {listBoilerType}
+    </div>
+  );
 }
 
 BoilerType.propTypes = {
-  boilerList: PropTypes.array.isRequired,
-  delBoilerType: PropTypes.array.isRequired,
-  updateBoilerType: PropTypes.array.isRequired,
+  boilerTypes: PropTypes.array.isRequired,
 }
 
 export default BoilerType;
