626fc34460d4f253a7c822faf01b6d1e954c6a75
Blas Medina <blasmedina5@gmail.com>
2020-12-31 20:22:55 -0300

    CAL-61: connect appointments actions

--- src/redux/reducers/appointmentsReducer.js
+++ src/redux/reducers/appointmentsReducer.js
@@ -1,31 +1,110 @@
 import {
-  ADD_APPOINTMENT,
-  DELETE_APPOINTMENT,
-  UPDATE_APPOINTMENT,
+  GET_APPOINTMENT_FETCHING,
+  GET_APPOINTMENT_FULFILLED,
+  GET_APPOINTMENT_REJECTED,
+  ADD_APPOINTMENT_FETCHING,
+  ADD_APPOINTMENT_FULFILLED,
+  ADD_APPOINTMENT_REJECTED,
+  DELETE_APPOINTMENT_FETCHING,
+  DELETE_APPOINTMENT_FULFILLED,
+  DELETE_APPOINTMENT_REJECTED,
+  UPDATE_APPOINTMENT_FULFILLED,
+  UPDATE_APPOINTMENT_REJECTED,
+  UPDATE_APPOINTMENT_FETCHING
 } from "../types/appointmentTypes";
-import appointment from "../../mocks/mocksAppointment.json";
 
-const appointmentReducer = (state = appointment, action) => {
+const initialState = {
+  isLoading: false,
+  list: [],
+  error: false
+};
+
+const appointmentReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_APPOINTMENT: {
-      return [...state, action.payload];
+    case GET_APPOINTMENT_FETCHING: {
+      return {
+        ...state,
+        isLoading: true,
+        error: false
+      };
     }
-    case DELETE_APPOINTMENT: {
-      return [...state.filter((appoint) => appoint.id !== action.payload)];
+    case GET_APPOINTMENT_FULFILLED: 
+      return {
+        ...state,
+        isLoading: false,
+        list: action.payload
+    };
+    case GET_APPOINTMENT_REJECTED: 
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+    };
+    case ADD_APPOINTMENT_FETCHING:
+      return  {
+        ...state,
+        isLoading: true,
+        error: false
+      };
+    case ADD_APPOINTMENT_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        list: [...state.list, action.payload]
+      };
+    case ADD_APPOINTMENT_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+    case DELETE_APPOINTMENT_FETCHING:
+      return {
+        ...state,
+        isLoading: true,
+        error: false
+      };
+    case DELETE_APPOINTMENT_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        list: [...state.list.filter(appointments => appointments._id !== action.payload)]
+      };
+    case DELETE_APPOINTMENT_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case UPDATE_APPOINTMENT_FETCHING: {
+      return {
+          ...state,
+          isLoading: true,
+          error: false,
+      }
     }
-    case UPDATE_APPOINTMENT: {
-      return [
-        ...state.map((appoint) => {
-          if (appoint.id === action.payload.id) {
-            appoint = action.payload;
-          }
-          return appoint;
-        }),
-      ];
+    case UPDATE_APPOINTMENT_FULFILLED: {
+        return {
+            ...state,
+            isLoading: false,
+            list: [...state.list.map(appointments => {
+                if (appointments._id === action.payload._id) {
+                  appointments = action.payload;
+                }
+                return appointments;
+            })]
+        }
     }
-    default: {
-      return state;
+    case UPDATE_APPOINTMENT_REJECTED: {
+        return {
+            ...state,
+            isLoading: false,
+            error: action.payload
+        }
     }
+
+    default:
+      return state ;
   }
 };
 
