ce0aaff0919224813b3449b7ffdf791409727a2d
Alexandra Maero <alexandramaero12@gmail.com>
2021-01-02 10:24:35 -0300

    CAL-64: resolve conflicts in eslintcache

--- src/redux/types/boilerTypes.js
+++ src/redux/types/boilerTypes.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 export const ADD_BOILER = "ADD_BOILER";
 export const DELETE_BOILER = "DELETE_BOILER";
 export const EDIT_BOILER = "EDIT_BOILER";
+=======
+  
+export const ADD_BOILER = "ADD_BOILER";
+export const DELETE_BOILER = "DELETE_BOILER";
+export const EDIT_BOILER = "EDIT_BOILER";
+>>>>>>> 43c33db... add types, actions and reducers for boiler
