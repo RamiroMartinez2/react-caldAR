7ebf1e44a038e3c46efdde5588e255f3423f54ad
cocolobos <nicolasmlobos@gmail.com>
2021-01-04 14:00:52 -0300

    CAL-67-Tech

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Footer from "../Footer/Footer";
 const Home = () => {
   return (
     <div className="title">
       <h1>CaldAR</h1>
       <h2>The Best Choice</h2>
+      <Footer/>
     </div>
   );
 };
