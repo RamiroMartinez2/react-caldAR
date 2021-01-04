439dd510d9a82899810dcd12f87848c77bdaa650
cocolobos <nicolasmlobos@gmail.com>
2021-01-04 13:53:17 -0300

    CAL-67-Tech

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Footer.module.css'
+
+function Footer() {
+  return (
+    <header >
+      <div className={style.footerStyle}>
+        <span>CaldAr The Best Choice, © Copyright 2021</span>
+      </div>
+    </header>
+  );
+}
+
+export default Footer;
