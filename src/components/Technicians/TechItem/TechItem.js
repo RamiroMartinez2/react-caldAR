8b028dfac448316b352a476147d7aa5246716348
cocolobos <nicolasmlobos@gmail.com>
2021-01-07 17:31:33 -0300

    CAL-Technician

--- src/components/Technicians/TechItem/TechItem.js
+++ src/components/Technicians/TechItem/TechItem.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import style from "./TechItem.module.css";
 import { BiPencil } from "react-icons/bi";
-import { FcCancel } from "react-icons/fc";
-import { AiOutlineCheckCircle } from "react-icons/ai";
 import { GoTrashcan } from "react-icons/go";
 import Modal from "../../Modal/Modal";
 
@@ -15,10 +13,12 @@ const TechItem = (props) => {
   const cancelClick = () => {
     toggleEditing();
     setTech(props.tech);
+    setOpenModal(false)
   };
 
   const toggleEdit = () => {
     toggleEditing(!isEditing);
+    setOpenModal(true);
   };
 
   const onChange = (e) => {
@@ -32,79 +32,101 @@ const TechItem = (props) => {
 
   if (isEditing) {
     return (
-      <ul className={style.showForm}>
-        <input
-          className={style.inputStyleEdt}
-          type="text"
-          name="Number"
-          value={tech._id}
-          readOnly
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="text"
-          name="fullName"
-          placeholder=" Add Name"
-          value={tech.fullName}
-          onChange={onChange}
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="email"
-          name="email"
-          placeholder=" Add Email"
-          value={tech.email}
-          onChange={onChange}
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="number"
-          name="phone"
-          placeholder=" Add Phone"
-          value={tech.phone}
-          onChange={onChange}
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="text"
-          name="statusActive"
-          placeholder=" Add Status"
-          value={tech.statusActive}
-          onChange={onChange}
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="text"
-          name="trained"
-          placeholder=" Add Trained Skills"
-          value={tech.trained}
-          onChange={onChange}
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="number"
-          name="assignedClients"
-          placeholder=" Add Asigned Clients"
-          value={tech.assignedClients}
-          onChange={onChange}
-        ></input>
-        <input
-          className={style.inputStyleEdt}
-          type="number"
-          name="spareHoursAvailable"
-          placeholder=" Add Spare Hours Available"
-          value={tech.spareHoursAvailable}
-          onChange={onChange}
-        ></input>
-        <div>
-          <button onClick={cancelClick} className={style.Btn}>
-            <FcCancel />
-          </button>
-          <button onClick={saveChanges} className={style.Btn}>
-            <AiOutlineCheckCircle />
-          </button>
+      <Modal title="Editing Technician" openModal={openModal} setOpenModal={setOpenModal}>
+        <ul className={style.showForm}>
+          <div>
+          <label>ID</label>
+          <input
+            className={style.inputStyleEdt}
+            type="text"
+            name="Number"
+            value={tech._id}
+            readOnly
+          ></input>
+          </div>
+          <div>
+          <label>Full Name</label>
+          <input
+            className={style.inputStyleEdt}
+            type="text"
+            name="fullName"
+            placeholder=" Add Name"
+            value={tech.fullName}
+            onChange={onChange}
+          ></input>
+          </div>
+          <div>
+          <label>Email</label>
+          <input
+            className={style.inputStyleEdt}
+            type="email"
+            name="email"
+            placeholder=" Add Email"
+            value={tech.email}
+            onChange={onChange}
+          ></input>
+          </div>
+          <div>
+          <label>Phone</label>
+          <input
+            className={style.inputStyleEdt}
+            type="number"
+            name="phone"
+            placeholder=" Add Phone"
+            value={tech.phone}
+            onChange={onChange}
+          ></input>
+          </div>
+          <div>
+          <label>Status</label>
+          <input
+            className={style.inputStyleEdt}
+            type="text"
+            name="statusActive"
+            placeholder=" Add Status"
+            value={tech.statusActive}
+            onChange={onChange}
+          ></input>
+          </div>
+          <div>
+          <label>Trained Skills</label>
+          <input
+            className={style.inputStyleEdt}
+            type="text"
+            name="trained"
+            placeholder=" Add Trained Skills"
+            value={tech.trained}
+            onChange={onChange}
+          ></input>
+          </div>
+          <div>
+          <label>Assigned Clients</label>
+          <input
+            className={style.inputStyleEdt}
+            type="number"
+            name="assignedClients"
+            placeholder=" Add Assigned Clients"
+            value={tech.assignedClients}
+            onChange={onChange}
+          ></input>
+          </div>
+          <div>
+          <label>Hours Available</label>
+          <input
+            className={style.inputStyleEdt}
+            type="number"
+            name="spareHoursAvailable"
+            placeholder=" Add Spare Hours Available"
+            value={tech.spareHoursAvailable}
+            onChange={onChange}
+          ></input>
+          </div>     
+        <div className={style.groupModal}>
+          <button onClick={saveChanges} className={style.BtnModCheck}>Accept</button>
+          <button onClick={cancelClick} className={style.BtnModCancel}>Cancel</button>
         </div>
       </ul>
+      </Modal>
     );
   }
   return (
@@ -118,13 +140,13 @@ const TechItem = (props) => {
       <li className={style.liStyle}>{props.tech.assignedClients}</li>
       <li className={style.liStyle}>{props.tech.spareHoursAvailable}</li>
       <div>
+      <button onClick={toggleEdit} className={style.Btn}><BiPencil/></button>
       <button className={style.Btn} onClick={() => setOpenModal(true)}><GoTrashcan /></button>
         <Modal openModal={openModal} setOpenModal={setOpenModal}>
           <p className={style.msgConfirm}>Are you sure you want to delete ?</p>
           <button className={style.btnSubmit} onClick={() => props.deleteTechnician(props.tech._id)}>{" "}Confirm{" "}</button>
           <button className={style.btnSubmit} onClick={() => setOpenModal(false)}>{" "}Cancel{" "}</button>
         </Modal>
-      <button onClick={toggleEdit} className={style.Btn}><BiPencil/></button>
       </div>
     </ul>
   );
